40b400fa872bffc26b7a7e3fa2882505694a880d
Diego Vieira <dvsdiegovieira@gmail.com>
2023-11-23 17:03:20 -0300

    update: models

--- app.py
+++ app.py
@@ -18,7 +18,13 @@ from routes.medico import medico_bp
 from routes.modelo import modelo_bp
 from models import Pessoa
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required
-
+import base64
+import scipy as sp
+from scipy import ndimage
+from tensorflow import keras
+from keras.models import  Model
+from PIL import Image
+import io
 app = Flask(__name__)
 
 CORS(app, origins='*')
@@ -27,11 +33,28 @@ app.config['JWT_SECRET_KEY'] = 'hermes123'
 jwt = JWTManager(app)
 
 # Carrega o modelo .h5
-model1 = tf.keras.models.load_model('./modeloXception.h5')
-model2 = tf.keras.models.load_model('./CNN_modelvgg19.h5')
+# model1 = tf.keras.models.load_model('./modeloXception.h5')
+# model2 = tf.keras.models.load_model('./CNN_modelvgg19.h5')
+model1 = tf.keras.models.load_model('./model-13-0.9788-27092023.h5')
 models = []
 models.append(model1)
-models.append(model2)
+
+
+gap_weights = model1.layers[-1].get_weights()[0]
+cam_model  = Model(inputs=[model1.input], outputs=[model1.layers[-8].output, model1.output])
+
+def cam_result(features, results) -> tuple:
+  # there is only one image in the batch so we index at `0`
+  features_for_img = features[0]
+  prediction = results[0][0]
+  # there is only one unit in the output so we get the weights connected to it
+  class_activation_weights = gap_weights[:,0]
+  # upsample to the image size
+  class_activation_features = sp.ndimage.zoom(features_for_img, (224/7, 224/7, 1), order=2)
+  #spline interpolation of order = 2 (G search)
+  # compute the intensity of each feature in the CAM
+  cam_output  = np.dot(class_activation_features, class_activation_weights)
+  return prediction, cam_output
 
 db_user = 'postgres'
 db_password = '123'
@@ -65,16 +88,39 @@ def predict(model_id):
         image = tf.image.resize(image, (224, 224))
         image = tf.expand_dims(image, axis=0) 
 
-        # image = tf.image.rgb_to_grayscale(image)
-
-        image = tf.cast(image, tf.float32) / 255.0
-        model = models[model_id - 1]
      
-        predictions = model.predict(image)
-        predictions = predictions.tolist()
-        print(predictions)
-        data = {'predictions': predictions}
+        
+        if model_id == 1:
+            image = tf.image.rgb_to_grayscale(image)
+            image = tf.cast(image, tf.float32) / 255.0
+            features, results = cam_model.predict(image)
+            result, map_act = cam_result(features, results)
+        # Converter ndarray para uma imagem PIL no modo 'RGB'
+            image_pil = Image.fromarray(map_act.astype('uint8')).convert('RGB')
+
+            # Salvar a imagem PIL em um buffer de bytes
+            img_byte_array = io.BytesIO()
+            image_pil.save(img_byte_array, format='JPEG')
+
+            # Converter o buffer de bytes para base64
+            imagem_base64 = base64.b64encode(img_byte_array.getvalue()).decode('utf-8')
+            data = {'predictions': result.tolist(), 'image':imagem_base64}
+            print(data.get('predictions'))
+            
+        else:
+            model = models[model_id - 1]
+            image = tf.cast(image, tf.float32) / 255.0
+            predictions = model.predict(image)
+            predictions = predictions.tolist()
+            data = {'predictions': predictions}
+
+
         response = jsonify(data)
+
+        print('foi aqui')
+        if model_id == 3:
+            response.headers['Content-Disposition'] = 'attachment; filename=image.bin'
+            response.headers['Content-Type'] = 'image/octet-stream'
         response.headers['Access-Control-Allow-Origin'] = '*'
         return response
     except Exception as e:
