5f3cbc71a35f1e46346829cc07108a76bfaf85de
Diego Vieira <dvsdiegovieira@gmail.com>
2023-11-24 16:10:50 -0300

    update: model

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ jwt = JWTManager(app)
 # Carrega o modelo .h5
 # model1 = tf.keras.models.load_model('./modeloXception.h5')
 # model2 = tf.keras.models.load_model('./CNN_modelvgg19.h5')
-model1 = tf.keras.models.load_model('./model-13-0.9788-27092023.h5', custom_objects={'Custom>Adam': tf.optimizers.Adam})
+model1 = tf.keras.models.load_model('./model-13-0.9788-27092023.h5', custom_objects=None, compile=True, options=None)
 models = []
 models.append(model1)
 
