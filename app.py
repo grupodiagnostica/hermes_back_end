af8c87c0bfdd14d05e74174940ab088cebf90eb0
Diego Vieira <dvsdiegovieira@gmail.com>
2023-11-26 12:05:30 -0300

    update: to deploy

--- app.py
+++ app.py
@@ -83,27 +83,27 @@ jwt = JWTManager(app)
 # Carrega o modelo .h5
 model1 = tf.keras.models.load_model('./modeloXception.h5')
 # model2 = tf.keras.models.load_model('./CNN_modelvgg19.h5')
-# model2 = tf.keras.models.load_model('./model-13-0.9788-27092023.h5', custom_objects={'Custom>Adam': custom_optimizer}, compile=True, options=None)
+model2 = tf.keras.models.load_model('./model-13-0.9788-27092023.h5')
 models = []
 models.append(model1)
-# models.append(model2)
+models.append(model2)
 
 
-# gap_weights = model2.layers[-1].get_weights()[0]
-# cam_model  = tf.keras.models.Model(inputs=[model2.input], outputs=[model2.layers[-8].output, model2.output])
+gap_weights = model2.layers[-1].get_weights()[0]
+cam_model  = tf.keras.models.Model(inputs=[model2.input], outputs=[model2.layers[-8].output, model2.output])
 
-# def cam_result(features, results) -> tuple:
-#   # there is only one image in the batch so we index at `0`
-#   features_for_img = features[0]
-#   prediction = results[0][0]
-#   # there is only one unit in the output so we get the weights connected to it
-#   class_activation_weights = gap_weights[:,0]
-#   # upsample to the image size
-#   class_activation_features = sp.ndimage.zoom(features_for_img, (224/7, 224/7, 1), order=2)
-#   #spline interpolation of order = 2 (G search)
-#   # compute the intensity of each feature in the CAM
-#   cam_output  = np.dot(class_activation_features, class_activation_weights)
-#   return prediction, cam_output
+def cam_result(features, results) -> tuple:
+  # there is only one image in the batch so we index at `0`
+  features_for_img = features[0]
+  prediction = results[0][0]
+  # there is only one unit in the output so we get the weights connected to it
+  class_activation_weights = gap_weights[:,0]
+  # upsample to the image size
+  class_activation_features = sp.ndimage.zoom(features_for_img, (224/7, 224/7, 1), order=2)
+  #spline interpolation of order = 2 (G search)
+  # compute the intensity of each feature in the CAM
+  cam_output  = np.dot(class_activation_features, class_activation_weights)
+  return prediction, cam_output
 
 db_user = 'hermesuser'
 db_password = '123'
@@ -139,35 +139,30 @@ def predict(model_id):
 
      
         
-        # if model_id == 2:
-        #     image = tf.image.rgb_to_grayscale(image)
-        #     image = tf.cast(image, tf.float32) / 255.0
-        #     features, results = cam_model.predict(image)
-        #     result, map_act = cam_result(features, results)
-        # # Converter ndarray para uma imagem PIL no modo 'RGB'
-        #     image_pil = Image.fromarray(map_act.astype('uint8')).convert('RGB')
+        if model_id == 2:
+            image = tf.image.rgb_to_grayscale(image)
+            image = tf.cast(image, tf.float32) / 255.0
+            features, results = cam_model.predict(image)
+            result, map_act = cam_result(features, results)
+        # Converter ndarray para uma imagem PIL no modo 'RGB'
+            image_pil = Image.fromarray(map_act.astype('uint8')).convert('RGB')
 
-        #     # Salvar a imagem PIL em um buffer de bytes
-        #     img_byte_array = io.BytesIO()
-        #     image_pil.save(img_byte_array, format='JPEG')
+            # Salvar a imagem PIL em um buffer de bytes
+            img_byte_array = io.BytesIO()
+            image_pil.save(img_byte_array, format='JPEG')
 
-        #     # Converter o buffer de bytes para base64
-        #     imagem_base64 = base64.b64encode(img_byte_array.getvalue()).decode('utf-8')
-        #     data = {'predictions': result.tolist(), 'image':imagem_base64}
-        #     print(data.get('predictions'))
+            # Converter o buffer de bytes para base64
+            imagem_base64 = base64.b64encode(img_byte_array.getvalue()).decode('utf-8')
+            data = {'predictions': result.tolist(), 'image':imagem_base64}
+            print(data.get('predictions'))
             
-        # else:
-        #     model = models[model_id - 1]
-        #     image = tf.cast(image, tf.float32) / 255.0
-        #     predictions = model.predict(image)
-        #     predictions = predictions.tolist()
-        #     data = {'predictions': predictions}
+        else:
+            model = models[model_id - 1]
+            image = tf.cast(image, tf.float32) / 255.0
+            predictions = model.predict(image)
+            predictions = predictions.tolist()
+            data = {'predictions': predictions}
 
-        model = models[model_id - 1]
-        image = tf.cast(image, tf.float32) / 255.0
-        predictions = model.predict(image)
-        predictions = predictions.tolist()
-        data = {'predictions': predictions}
 
         response = jsonify(data)
 
