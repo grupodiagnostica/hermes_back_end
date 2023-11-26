c68e2decb80be46549b1b2bf7901edff40bbed25
Diego Vieira <dvsdiegovieira@gmail.com>
2023-11-25 22:39:19 -0300

    update: app para teste

--- app.py
+++ app.py
@@ -81,16 +81,16 @@ custom_optimizer = tf.keras.optimizers.Adam(
 
 
 # Carrega o modelo .h5
-# model1 = tf.keras.models.load_model('./model-13-0.9788-27092023.h5')
+model1 = tf.keras.models.load_model('./model-1.h5')
 # model2 = tf.keras.models.load_model('./CNN_modelvgg19.h5')
-model2 = tf.keras.models.load_model('./model-13-0.9788-27092023.h5', custom_objects={'Custom>Adam': custom_optimizer}, compile=True, options=None)
+# model2 = tf.keras.models.load_model('./model-13-0.9788-27092023.h5', custom_objects={'Custom>Adam': custom_optimizer}, compile=True, options=None)
 models = []
-# models.append(model1)
-models.append(model2)
+models.append(model1)
+# models.append(model2)
 
 
-gap_weights = model2.layers[-1].get_weights()[0]
-cam_model  = tf.keras.models.Model(inputs=[model2.input], outputs=[model2.layers[-8].output, model2.output])
+# gap_weights = model2.layers[-1].get_weights()[0]
+# cam_model  = tf.keras.models.Model(inputs=[model2.input], outputs=[model2.layers[-8].output, model2.output])
 
 def cam_result(features, results) -> tuple:
   # there is only one image in the batch so we index at `0`
@@ -139,29 +139,34 @@ def predict(model_id):
 
      
         
-        if model_id == 2:
-            image = tf.image.rgb_to_grayscale(image)
-            image = tf.cast(image, tf.float32) / 255.0
-            features, results = cam_model.predict(image)
-            result, map_act = cam_result(features, results)
-        # Converter ndarray para uma imagem PIL no modo 'RGB'
-            image_pil = Image.fromarray(map_act.astype('uint8')).convert('RGB')
+        # if model_id == 2:
+        #     image = tf.image.rgb_to_grayscale(image)
+        #     image = tf.cast(image, tf.float32) / 255.0
+        #     features, results = cam_model.predict(image)
+        #     result, map_act = cam_result(features, results)
+        # # Converter ndarray para uma imagem PIL no modo 'RGB'
+        #     image_pil = Image.fromarray(map_act.astype('uint8')).convert('RGB')
 
-            # Salvar a imagem PIL em um buffer de bytes
-            img_byte_array = io.BytesIO()
-            image_pil.save(img_byte_array, format='JPEG')
+        #     # Salvar a imagem PIL em um buffer de bytes
+        #     img_byte_array = io.BytesIO()
+        #     image_pil.save(img_byte_array, format='JPEG')
 
-            # Converter o buffer de bytes para base64
-            imagem_base64 = base64.b64encode(img_byte_array.getvalue()).decode('utf-8')
-            data = {'predictions': result.tolist(), 'image':imagem_base64}
-            print(data.get('predictions'))
+        #     # Converter o buffer de bytes para base64
+        #     imagem_base64 = base64.b64encode(img_byte_array.getvalue()).decode('utf-8')
+        #     data = {'predictions': result.tolist(), 'image':imagem_base64}
+        #     print(data.get('predictions'))
             
-        else:
-            model = models[model_id - 1]
-            image = tf.cast(image, tf.float32) / 255.0
-            predictions = model.predict(image)
-            predictions = predictions.tolist()
-            data = {'predictions': predictions}
+        # else:
+        #     model = models[model_id - 1]
+        #     image = tf.cast(image, tf.float32) / 255.0
+        #     predictions = model.predict(image)
+        #     predictions = predictions.tolist()
+        #     data = {'predictions': predictions}
+        model = models[model_id - 1]
+        image = tf.cast(image, tf.float32) / 255.0
+        predictions = model.predict(image)
+        predictions = predictions.tolist()
+        data = {'predictions': predictions}
 
 
         response = jsonify(data)
