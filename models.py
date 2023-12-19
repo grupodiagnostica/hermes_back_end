7018c736c960ef96f0ce6b5f4ab6766255379d1c
Diego Vieira <dvsdiegovieira@gmail.com>
2023-12-19 11:12:03 -0300

    update: medico clinica realação

--- models.py
+++ models.py
@@ -55,11 +55,18 @@ class Paciente(db.Model):
         self.numero = numero
         self.estado = estado
 
+medico_clinica_association = db.Table(
+    'medico_clinica_association',
+    db.Column('medico_id', db.String(36), db.ForeignKey('medico.id')),
+    db.Column('clinica_id', db.String(36), db.ForeignKey('clinica.id'))
+)
+
 class Clinica(db.Model):
     id = db.Column(db.String(36), primary_key=True, default=str(uuid.uuid4()), unique=True, nullable=False)
     cnpj = db.Column(db.String(18), unique=True, nullable=False)
     nome = db.Column(db.String(100), nullable=False)
     foto_perfil = db.Column(db.Text, nullable=True)
+    medicos = db.relationship('Medico', secondary=medico_clinica_association, backref='clinica')
     funcionarios = db.relationship('Funcionario', backref='clinica', lazy=True)
     def __init__(self, cnpj, nome, id=None, foto_perfil=None):
         if id is None:
@@ -72,6 +79,35 @@ class Clinica(db.Model):
             self.foto_perfil = foto_perfil
         self.cnpj = cnpj
         self.nome = nome
+
+
+class Medico(db.Model):
+    id = db.Column(db.String(36), primary_key=True, default=str(uuid.uuid4()), unique=True, nullable=False)
+    id_pessoa = db.Column(db.String(36), db.ForeignKey('pessoa.id'), nullable=False)
+    crm = db.Column(db.String(15), unique=True, nullable=False)
+    especialidade = db.Column(db.String(100))
+    senha = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    foto_perfil = db.Column(db.Text, nullable=True)
+    verification_code = db.Column(db.String(6), nullable=True)
+    verification_code_expiration = db.Column(db.DateTime, nullable=True)
+    clinicas = db.relationship('Clinica', secondary=medico_clinica_association, backref='medico')
+    diagnosticos = db.relationship('Diagnostico', backref='medico', lazy=True)
+    pacientes = db.relationship('Paciente', backref='medico', lazy=True)
+    def __init__(self, id_pessoa, crm, especialidade,senha,email, id=None, foto_perfil=None):
+        if id is None:
+            self.id = str(uuid.uuid4())
+        else:
+            self.id = id
+        if  foto_perfil is None:
+            self.foto_perfil = None
+        else:
+            self.foto_perfil = foto_perfil
+        self.id_pessoa = id_pessoa
+        self.crm = crm 
+        self.especialidade = especialidade
+        self.senha = senha
+        self.email = email
 
 class Funcionario(db.Model):
     id = db.Column(db.String(36), primary_key=True, default=str(uuid.uuid4()), unique=True, nullable=False)
@@ -91,33 +127,6 @@ class Funcionario(db.Model):
         self.senha = senha
         self.especialista = especialista
         self.id_clinica = id_clinica
-
-class Medico(db.Model):
-    id = db.Column(db.String(36), primary_key=True, default=str(uuid.uuid4()), unique=True, nullable=False)
-    id_pessoa = db.Column(db.String(36), db.ForeignKey('pessoa.id'), nullable=False)
-    crm = db.Column(db.String(15), unique=True, nullable=False)
-    especialidade = db.Column(db.String(100))
-    senha = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    foto_perfil = db.Column(db.Text, nullable=True)
-    verification_code = db.Column(db.String(6), nullable=True)
-    verification_code_expiration = db.Column(db.DateTime, nullable=True)
-    diagnosticos = db.relationship('Diagnostico', backref='medico', lazy=True)
-    pacientes = db.relationship('Paciente', backref='medico', lazy=True)
-    def __init__(self, id_pessoa, crm, especialidade,senha,email, id=None, foto_perfil=None):
-        if id is None:
-            self.id = str(uuid.uuid4())
-        else:
-            self.id = id
-        if  foto_perfil is None:
-            self.foto_perfil = None
-        else:
-            self.foto_perfil = foto_perfil
-        self.id_pessoa = id_pessoa
-        self.crm = crm 
-        self.especialidade = especialidade
-        self.senha = senha
-        self.email = email
 
 # -------------- doença não utilizada por equanto ---------------
 # class Doenca(db.Model):
