85086121015601931ac8eee0b3833eeceb39dc8d
Diego Vieira <dvsdiegovieira@gmail.com>
2024-02-01 10:54:18 -0300

    fix: erro no modelo em models

--- models.py
+++ models.py
@@ -204,7 +204,7 @@ class Modelo(db.Model):
     dataAugmentation = db.Column(db.Boolean, default=False)
     tipoImagem = db.Column(db.String(15))
 
-    def __init__(self, precisao, acuracia, f1score, recall, kappa, filtros, dataAugmentation, tipoImagem, id=None):
+    def __init__(self, precisao, acuracia, f1score, recall, kappa, filtros, dataAugmentation, tipoImagem, cnpj, id=None):
         if id is None:
             self.id = str(uuid.uuid4())
         else:
